0e7c819a7edae09fd0954e789b97f39e2e4c2549
Shairo Deladia <skdeladia@gmail.com>
2026-07-05 00:02:15 +0800

    Added Main and Calculator class

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        System.out.println("Hello World!");
+        System.out.println(Calculator.add(x,y));
+    }
+}
